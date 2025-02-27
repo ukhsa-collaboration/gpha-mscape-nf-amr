d1bb235c53179cb53e2fb20b8a6692cfe06c44d9
nfellaby <nfellaby@users.noreply.github.com>
2025-02-27 15:52:52 +0000

    dev: added python script for pulling climb records

---
+++ generate-sample-sheet.py
@@ -0,0 +1,45 @@
+import os
+from onyx import OnyxConfig, OnyxEnv, OnyxClient
+import argparse
+from pathlib import Path
+
+
+
+config = OnyxConfig(
+    domain=os.environ[OnyxEnv.DOMAIN],
+    token=os.environ[OnyxEnv.TOKEN],
+)
+
+def get_args() -> argparse.Namespace:
+    """
+    Command Line
+    :return: argParse variable
+    """
+    parser = argparse.ArgumentParser(description="generate-sample-sheet")
+    parser.add_argument("-i", "--input",
+                help="input file containing CLIMB-IDs",
+                required=True, type=Path)
+    parser.add_argument("-o", "--output",
+                help="Output directory for downloaded data",
+                required=True, type=Path)
+
+def get_record_by_climb_id(climb_id_list: list):
+    for climb_id in climb_id_list:
+        with OnyxClient(config) as client:
+            record = client.get("project", climb_id)
+            print(record)
+
+def parse_file(fp: Path):
+    """
+    Read in text file, seperate climb ids into lis
+    :args: filepath str()
+    """
+    with open(fp, "r") as file:
+        data = file.readlines()  # Reads lines into a list
+        data = [line.strip() for line in data]  # Remove newline characters
+        return data
+
+def main():
+    args = get_args()
+    climb_id_list = parse_file(args.input)
+    get_record_by_climb_id(climb_id_list)
