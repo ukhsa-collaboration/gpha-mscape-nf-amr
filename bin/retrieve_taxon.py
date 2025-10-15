70bcfa466f2c158a19656aedb720e82a0e21968b
nfellaby <nfellaby@users.noreply.github.com>
2025-10-15 10:32:04 +0100

    test: error in python script

--- bin/retrieve_taxon.py
+++ bin/retrieve_taxon.py
@@ -59,15 +59,15 @@ def add_species(df_tsv: pd.DataFrame, taxid_dict: dict):
     :return: df_tsv with left joined 'name'
     '''
     # Convert dictionary into pandas dataframe
-    taxid_df = pd.DataFrame.from_dict(taxid_dict, orient='index')
-    breakpoint()
-    taxid_df.reset_index(inplace=True)
-    taxid_df.rename(columns={'index': 'taxid_key'}, inplace=True)
-    for df in [df_tsv, taxid_df]:
-        df['taxid'] = df['taxid'].astype(str)
-    df_merged  = df_tsv.merge(taxid_df[['taxid','name','raw_rank','rank']],
-                              on='taxid', how='left')
-    return df_merged
+    print(taxid_dict)
+    # taxid_df = pd.DataFrame.from_dict(taxid_dict, orient='index')
+    # taxid_df.reset_index(inplace=True)
+    # taxid_df.rename(columns={'index': 'taxid_key'}, inplace=True)
+    # for df in [df_tsv, taxid_df]:
+    #     df['taxid'] = df['taxid'].astype(str)
+    # df_merged  = df_tsv.merge(taxid_df[['taxid','name','raw_rank','rank']],
+    #                           on='taxid', how='left')
+    # return df_merged
 
 def link_abricate_results(df_merged: pd.DataFrame, abricate_csv: Path):
     '''
