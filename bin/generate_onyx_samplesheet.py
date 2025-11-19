b7c8e1198d6ec1ec78d5fdefd4ab08e424a23ee2
nfellaby <nfellaby@users.noreply.github.com>
2025-11-19 16:10:11 +0000

    Dev Batch: Sample report, Linting, Depedencies (#25)
    * Report (#15)
    
    * feat: updated with revised report generation script
    
    * chore: tidied up imports
    
    * chore: removed un-used function
    
    * chore: tidied up code on pull request review
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * test: testing df key bug
    
    * Feature/requirements (#16)
    
    * feat: added uv.lock requirements and pyproject.toml
    
    * feat: added pre-commit file
    
    * Feature/ci (#21)
    
    * feat: github actions for tests and linting (ruff)
    
    * feat: added uv.lock file
    
    * feat: added pytest requirement
    
    * chore: removed un-used library
    
    * feat: added ruff and uv_build info to toml
    
    * feat: added author to toml
    
    * Chore/ruff linting (#24)
    
    * chore: removed unneccessary file
    
    * chore: linting for script
    
    * chore: linting for script
    
    * chore: fix logging calls
    
    * chore: linting for script
    
    * chore: linting for script
    
    * chore: linting for script
    
    * chore: linting for script
    
    * bug: path basename
    
    * chore: removed unused library
    
    * chore: added ignore statement for vscode folder
    
    * chore: removed placeholder file
    
    * doc: updated workflow diagram
    
    * doc: Added license
    
    * chore: linting

--- bin/generate_onyx_samplesheet.py
+++ bin/generate_onyx_samplesheet.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import os
-from onyx import OnyxConfig, OnyxEnv, OnyxClient
-from onyx_analysis_helper import onyx_analysis_helper_functions as oa
 import argparse
+import os
 from pathlib import Path
+
 import pandas as pd
+from onyx import OnyxClient, OnyxConfig, OnyxEnv
+from onyx_analysis_helper import onyx_analysis_helper_functions as oa
 
 config = OnyxConfig(
     domain=os.environ[OnyxEnv.DOMAIN],
@@ -19,15 +20,11 @@ def get_args() -> argparse.Namespace:
     :return: argParse variable
     """
     parser = argparse.ArgumentParser(description="generate-sample-sheet")
-    parser.add_argument("-i", "--id",
-                help="Sample ID.",
-                required=True, type=str)
-    parser.add_argument("-c", "--columns",
-                help="Columns from Onyx, should be comma seperated string.",
-                required=True, type=str)
-    parser.add_argument("-o", "--output",
-                help="Output filepath.",
-                required=True, type=Path)
+    parser.add_argument("-i", "--id", help="Sample ID.", required=True, type=str)
+    parser.add_argument(
+        "-c", "--columns", help="Columns from Onyx, should be comma seperated string.", required=True, type=str
+    )
+    parser.add_argument("-o", "--output", help="Output filepath.", required=True, type=Path)
     args = parser.parse_args()
     return args
 
@@ -42,23 +39,19 @@ def get_record(sample_id: str, columns: list) -> tuple[dict, int]:
         columns.append("climb_id")
 
     with OnyxClient(config) as client:
-            df = pd.DataFrame(client.filter(
-            project = "mscape",
-            climb_id = sample_id,
-            include = columns
-        ))
+        df = pd.DataFrame(client.filter(project="mscape", climb_id=sample_id, include=columns))
     exit_code = 0
 
-    #TODO: handle no records returned
+    # TODO: handle no records returned
     return df, exit_code
 
 
-def main():
+def main() -> None:
     args = get_args()
-    col_names = args.columns.split(',')
+    col_names = args.columns.split(",")
     df, exit_code = get_record(args.id, col_names)
     df.to_csv(args.output, index=False)
 
 
 if __name__ == "__main__":
-    main() 
+    main()
