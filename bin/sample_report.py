3381ff30fbca644e76d2e671f7c2cdd6699b9577
nfellaby <nfellaby@users.noreply.github.com>
2025-11-25 09:50:37 +0000

    test: enterez records

--- bin/sample_report.py
+++ bin/sample_report.py
@@ -99,10 +99,10 @@ def simplify_taxa(email: str, df: pd.DataFrame) -> pd.DataFrame:
             record = ET.fromstring(handle.read())
             # Extract lineage
             lineage_info = record.find(".//LineageEx")
-            print(lineage_info)
             species_name = None
             if lineage_info is not None:
                 for taxon in lineage_info:
+                    print(taxon)
                     rank = taxon.find("Rank").text
                     name = taxon.find("ScientificName").text
                     if rank == "species":
