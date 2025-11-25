db102665aa999387d9ea5773d2dcc57dbd6b361d
nfellaby <nfellaby@users.noreply.github.com>
2025-11-25 13:06:21 +0000

    feat: gene_coverage

--- bin/sample_report.py
+++ bin/sample_report.py
@@ -711,7 +711,7 @@ def generate_html_report(df: pd.DataFrame, output_path: str, sample_id: str, amr
         # Generate Coverage plots per species
 
         # Generate tables
-        coverage_tables = build_gene_coverage_table
+        coverage_tables = build_gene_coverage_table(df)
         print(coverage_tables)
 
         # for gene, group in df.groupby("GENE"):
