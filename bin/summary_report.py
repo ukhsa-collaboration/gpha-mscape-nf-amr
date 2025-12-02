0ce05072ad259cd5bea9557aad9c46a76c53d7b9
nfellaby <nfellaby@users.noreply.github.com>
2025-12-02 15:46:05 +0000

    feat: summary report - formatting data tables

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -108,8 +108,8 @@ def generate_summary_report(df: pd.DataFrame, metadata_file: str, output_dir: st
     merged_df = pd.merge(df, metadata, on="climb_id", how="left")
 
     # Format publish date
-    # merged_df["publish_date"] = pd.to_datetime(merged_df["publish_date"]).dt.strftime("%Y-%m-%d")
-    print(merged_df.columns)
+    merged_df["published_date"] = pd.to_datetime(merged_df["published_date"]).dt.strftime("%Y-%m-%d")
+    print(merged_df)
 
     logger.info("Summary report generated at %s", output_dir)
 
