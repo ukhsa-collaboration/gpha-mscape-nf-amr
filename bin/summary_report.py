5461dfc10a122407d2811b5173e34342b3c72ee3
nfellaby <nfellaby@users.noreply.github.com>
2025-12-02 15:44:35 +0000

    feat: summary report

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -108,7 +108,7 @@ def generate_summary_report(df: pd.DataFrame, metadata_file: str, output_dir: st
     merged_df = pd.merge(df, metadata, on="climb_id", how="left")
 
     # Format publish date
-    merged_df["publish_date"] = pd.to_datetime(merged_df["publish_date"]).dt.strftime("%Y-%m-%d")
+    # merged_df["publish_date"] = pd.to_datetime(merged_df["publish_date"]).dt.strftime("%Y-%m-%d")
     print(merged_df.columns)
 
     logger.info("Summary report generated at %s", output_dir)
