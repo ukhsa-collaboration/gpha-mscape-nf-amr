e9374ab364a3ed6147c3cd65fca2bdd5e223bb8d
nfellaby <nfellaby@users.noreply.github.com>
2025-12-02 17:17:05 +0000

    feat: summary report - summary stats - total number of samples, per week-year

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -243,18 +243,18 @@ def generate_summary_report(df: pd.DataFrame, metadata_file: str, output_dir: st
     df["climb_id"] = df["#FILE"].str.replace(".fastq", "", regex=False)
 
     # Load metadata
-    metadata_df = pd.read_csv(metadata_file, sep=",")
-    print(metadata_df["published_date"])
-    # metadata_df = format_dates(metadata_df, date_column="published_date")
+    metadata_df = pd.read_csv(metadata_file, sep=None)
 
-    # # Merge data with metadata
-    # merged_df = pd.merge(df, metadata_df, on="climb_id", how="left")
-    # logger.info("Merged data with metadata. Total records: %d", len(merged_df))
+    metadata_df = format_dates(metadata_df, date_column="published_date")
 
-    # # Generate Summary Statement for report
-    # summary_stats(merged_df, metadata_df, output_dir)
+    # Merge data with metadata
+    merged_df = pd.merge(df, metadata_df, on="climb_id", how="left")
+    logger.info("Merged data with metadata. Total records: %d", len(merged_df))
 
-    # logger.info("Summary report generated at %s", output_dir)
+    # Generate Summary Statement for report
+    summary_stats(merged_df, metadata_df, output_dir)
+
+    logger.info("Summary report generated at %s", output_dir)
 
 
 def main() -> None:
@@ -264,7 +264,7 @@ def main() -> None:
     log_file = Path(args.output_dir, "amr_html_summary_report_log.txt")
     set_up_logger(log_file)
     logger.info("Starting summary report generation.")
-    df = pd.read_csv(args.input_tsv, sep="\t")
+    df = pd.read_csv(args.input_tsv, sep=None)
     logger.info("Simplifying taxa using Enterez.")
     df = simplify_taxa(args.email, df)
     generate_summary_report(df, args.metadata, args.output_dir)
