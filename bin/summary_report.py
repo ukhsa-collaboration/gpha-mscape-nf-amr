339740557371e5fed895e65254e67331d86e6b02
nfellaby <nfellaby@users.noreply.github.com>
2025-12-03 05:35:53 +0000

    feat: summary report - summary stats - total number of samples, per week-year

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -217,8 +217,7 @@ def summary_stats(df: pd.DataFrame, metadata_df: pd.DataFrame, output_dir: str) 
     num_amr_samples = df["climb_id"].nunique()
     # percentage of samples with AMR annotations to 2 decim
     per_amr_samples = (num_amr_samples / total_samples) * 100
-    per_amr_samples = round(per_amr_samples, 2)
-    logger.info("Number of samples with AMR annotations: %d (%f%)", (num_amr_samples, per_amr_samples))
+    logger.info("Number of samples with AMR annotations: %d (%.2f%%)", num_amr_samples, per_amr_samples)
     # Create a dataframe where the first column is the climb id, the second is the domain, and the third is the number of reads
     summary_df = df.groupby(["climb_id", "domain"]).size().reset_index(name="amr_hit_count")
     # From the summary_df, create a summary table that shows the min, max, median, and mean number of amr hits per domain
