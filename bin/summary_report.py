f3742cddd5b86c314ba2eeef65bcba0aa48d6b96
nfellaby <nfellaby@users.noreply.github.com>
2025-12-03 05:43:40 +0000

    feat: summary report - clean up

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -150,7 +150,6 @@ def domain_amr_read_counts(df: pd.DataFrame, output_dir: str) -> pd.DataFrame:
             .reset_index(name="unique_amr_sequence_count")
         )
         # Generate a box-whisker plot showing the distribution of unique_amr_sequence_count per species with plotly
-        print(species_sample_amr_reads)
 
         fig = px.box(
             species_sample_amr_reads,
