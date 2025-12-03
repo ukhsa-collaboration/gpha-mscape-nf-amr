cf7037622c90cea606f5cf8a76deaedfb8fd3892
nfellaby <nfellaby@users.noreply.github.com>
2025-12-03 11:37:57 +0000

    feat: summary report - summary stats - summarise class

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -253,6 +253,7 @@ def summarize_by_class(df: pd.DataFrame, output_dir: str) -> pd.DataFrame:
         # Create presence columns for each resistance class per sample-week
         df_presence = df_domain[["climb_id", "epi_week_year"]].drop_duplicates().copy()
         print(df_presence)
+        print(df_domain.groupby(["#FILE", "epi_week_year"])["RESISTANCE_LIST"])
         for r in unique_resistances:
             df_presence[r] = (
                 df_domain.groupby(["climb_id", "epi_week_year"])["RESISTANCE_LIST"]
