b79a83a580fc1411e5f6d3539c4b36c2f54dac93
nfellaby <nfellaby@users.noreply.github.com>
2026-03-17 15:49:40 +0000

    feat: write to output dataframe

--- bin/get_taxa_of_interest.py
+++ bin/get_taxa_of_interest.py
@@ -205,7 +205,7 @@ def main(args) -> None:
     species = get_species_names(args.reference_taxa_list)
     species_match_df = get_taxa_id(species, args.taxaplease_db)
     species_match_df = get_parent_taxid(species_match_df, args.taxaplease_db)
-    output_fn = Path(args.output_dir) / str("_taxaplease_reference_table.tsv")
+    output_fn = Path(args.output_dir) / str("taxaplease_reference_table.tsv")
     species_match_df.to_csv(output_fn, index=False, sep="\t")
 
 
